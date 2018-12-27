cf647ed4fc5e18e44de6714dcd131fc2f700c208
tallpol3 <bartek.biskupski@gmail.com>
2018-12-27 14:02:53 +0100

    change email setup for heroku demo

--- testproject/demo.py
+++ testproject/demo.py
@@ -132,14 +132,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Console backend
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST', default='')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
+EMAIL_PORT = env('EMAIL_PORT', default=587)
+EMAIL_USE_TLS = env.bool('TLS', default=True)
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='code@merixstudio.com')
 
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
@@ -151,10 +157,10 @@ JWT_AUTH = {
     ),
 }
 
-SIMPLE_JWT = {
-    'USER_ID_FIELD': 'username',
-    'USER_ID_CLAIM': 'username',
-}
+# SIMPLE_JWT = {
+#     'USER_ID_FIELD': 'username',
+#     'USER_ID_CLAIM': 'username',
+# }
 
 DJOSER = {
     'SERIALIZERS': {
