712e99742be979683f711021d2b2c94b31ef6060
benzkji <bnzk@bnzk.ch>
2019-05-01 12:59:02 +0200

    remove self._action call, as its not always available

--- trench/views/authtoken.py
+++ trench/views/authtoken.py
@@ -1,19 +1,28 @@
-from djoser.views import TokenCreateView, TokenDestroyView
 from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from djoser.views import TokenCreateView, TokenDestroyView
+from djoser import utils
+from djoser.conf import settings
 
 from trench.views.base import MFACodeLoginMixin, MFACredentialsLoginMixin
 
 
 class ObtainAuthTokenMixin:
     def handle_user_login(self, serializer, *args, **kwargs):
-        return self._action(serializer)
+        token = utils.login_user(self.request, serializer.user)
+        token_serializer_class = settings.SERIALIZERS.token
+        return Response(
+            data=token_serializer_class(token).data,
+            status=status.HTTP_200_OK,
+        )
 
 
 class AuthTokenLoginOrRequestMFACode(MFACredentialsLoginMixin,
                                      ObtainAuthTokenMixin,
-                                     TokenCreateView,
                                      GenericAPIView):
     pass
+
 
 
 class AuthTokenLoginWithMFACode(MFACodeLoginMixin,
