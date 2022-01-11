431e63b5e230d2672f0b05e8fd8152d3ba3b308a
Edvard Ødegaard <god@grotto.faith>
2022-01-11 03:38:47 +0100

    Make interval / token period configurable and change default to 30

--- trench/command/create_otp.py
+++ trench/command/create_otp.py
@@ -1,10 +1,11 @@
 from pyotp import TOTP
+from trench.settings import trench_settings
 
 
 class CreateOTPCommand:
     @staticmethod
     def execute(secret: str) -> TOTP:
-        return TOTP(secret, interval=1)
+        return TOTP(secret, interval=trench_settings.TOKEN_PERIOD)
 
 
 create_otp_command = CreateOTPCommand.execute
